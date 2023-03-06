c85a0acdd9470c8ee914048940865f9ad0853e0b
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-06 16:19:50 +0700

    Add account information

--- lab01-terraform/providers.tf
+++ lab01-terraform/providers.tf
@@ -9,5 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "e2bf10f5-b611-44c7-9617-4a26610fc460"
+  client_id       = "504e75ea-1f30-4a71-a4af-a8aa3cb2fcd7"
+  client_secret   = "37L8Q~Z0l4Ng8tetYSngYRzeN2VuYs44LP1o7cxv"
+  tenant_id       = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 }
 
