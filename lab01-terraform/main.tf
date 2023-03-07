e9c77497d387dd100f8a4e4624845006e2c7229c
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-07 22:21:30 +0700

    Change subscription to create resources

--- lab01-terraform/main.tf
+++ lab01-terraform/main.tf
@@ -1,33 +1,33 @@
-resource "azurerm_resource_group" "lab01-rsg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
+# resource "azurerm_resource_group" "lab01-rsg" {
+#   name     = var.resource_group_name
+#   location = var.resource_group_location
+# }
 
 resource "azurerm_virtual_network" "lab01-virnet" {
   name                = "${var.resource_group_name}-VirNet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.lab01-rsg.location
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "lab01-rsg-subnet" {
   name = "${var.resource_group_name}-subnet"
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  resource_group_name = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.lab01-virnet.name
   address_prefixes = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "lab01-rsg-pubip" {
   name                = "Lab01-PubIP"
-  location            = azurerm_resource_group.lab01-rsg.location
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "lab01-lb" {
   name                = "Lab01-LoadBalancer"
-  location            = azurerm_resource_group.lab01-rsg.location
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
   frontend_ip_configuration {
     name                 = "LB-PublicIPAddress"
@@ -44,8 +44,8 @@ resource "azurerm_network_interface" "lab01-rsg-nic" {
   #count               = var.counts
   #name                = "${var.resource_group_name}-NIC-${count.index}"
   name                = "Lab01-NIC"
-  location            = azurerm_resource_group.lab01-rsg.location
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   
   ip_configuration {
     name                          = "${var.resource_group_name}-NIC-Internal"
@@ -56,8 +56,8 @@ resource "azurerm_network_interface" "lab01-rsg-nic" {
 
 resource "azurerm_network_security_group" "lab01-rsg-nsg" {
   name                = "lab01-rsg-NSG"
-  location            = azurerm_resource_group.lab01-rsg.location
-  resource_group_name = azurerm_resource_group.lab01-rsg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_network_security_rule" "lab01-rsg-nsr" {
@@ -70,7 +70,7 @@ resource "azurerm_network_security_rule" "lab01-rsg-nsr" {
   destination_port_range       = "*"
   source_address_prefix        = "*"
   destination_address_prefix   = "*"
-  resource_group_name          = azurerm_resource_group.lab01-rsg.name
+  resource_group_name          = var.resource_group_name
   network_security_group_name  = azurerm_network_security_group.lab01-rsg-nsg.name
 }
 
@@ -82,8 +82,8 @@ data "azurerm_image" "lab01-packer-img" {
 resource "azurerm_linux_virtual_machine" "lab01-rsg-linux-vm" {
   count                           = var.counts
   name                            = "${var.resource_group_name}-vm-${count.index}"
-  resource_group_name             = azurerm_resource_group.lab01-rsg.name
-  location                        = azurerm_resource_group.lab01-rsg.location
+  resource_group_name             = var.resource_group_name
+  location                        = var.resource_group_location
   size                            = var.size_vm
   admin_username                  = var.admin_usr
   admin_password                  = var.admin_pwd
