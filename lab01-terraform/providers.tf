a08a01ad6c9763a5320337cf21d686097d619882
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-07 22:21:30 +0700

    Add condition to create resources

--- lab01-terraform/providers.tf
+++ lab01-terraform/providers.tf
@@ -8,10 +8,14 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = "e2bf10f5-b611-44c7-9617-4a26610fc460"
-  client_id       = "504e75ea-1f30-4a71-a4af-a8aa3cb2fcd7"
-  client_secret   = "37L8Q~Z0l4Ng8tetYSngYRzeN2VuYs44LP1o7cxv"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "c761712a-772e-4832-b4d5-73b3a4b12e0b"
+  client_id       = "a0965861-c85c-4a46-82e5-265e8042ea66"
+  client_secret   = "_kn8Q~3tuOkIzDO5rolDR5cSPYjVDupACJr3Abi9"
   tenant_id       = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 }
 
