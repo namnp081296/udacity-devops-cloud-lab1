ac327a0aebfe58f84fe59d08cd5999db8ae65813
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-08 22:42:48 +0700

    Change the account information

--- lab01-terraform/providers.tf
+++ lab01-terraform/providers.tf
@@ -13,9 +13,9 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "c761712a-772e-4832-b4d5-73b3a4b12e0b"
-  client_id       = "a0965861-c85c-4a46-82e5-265e8042ea66"
-  client_secret   = "_kn8Q~3tuOkIzDO5rolDR5cSPYjVDupACJr3Abi9"
+  subscription_id = "456cc604-544c-45f8-99d0-c1b73aeec440"
+  client_id       = "099fd190-7e02-48c9-9147-c98f61234409"
+  client_secret   = ".rK8Q~f85VfgSQKEe4llRlBoqkup~57UYNLMbdgk"
   tenant_id       = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 }
 
