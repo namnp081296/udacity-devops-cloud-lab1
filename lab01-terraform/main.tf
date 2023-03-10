d66897572543e439b8725bf11a011eb1e033bdbf
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-10 13:13:48 +0700

    Add inbound, outbound rule for internal vm and allow http traffic to vm, also reconfig the number of vm

--- lab01-terraform/main.tf
+++ lab01-terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_availability_set" "lab01-avlset" {
 
 data "azurerm_availability_set" "lab01-avlset" {
   name                = var.availability_set_name
-  depends_on = [azurerm_availability_set.lab01-avlset]
+  depends_on          = [azurerm_availability_set.lab01-avlset]
   resource_group_name = var.resource_group_name
 }
 
@@ -55,25 +55,23 @@ resource "azurerm_lb_backend_address_pool" "lab01-lb-addrpool" {
 
 # Create NIC
 resource "azurerm_network_interface" "lab01-rsg-nic" {
-  # count               = var.counts
-  name                = "${var.resource_group_name}-NIC"
+  count               = var.counts
+  name                = "${var.resource_group_name}-NIC-${count.index}"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   
   ip_configuration {
-    name                          = "${var.resource_group_name}-NIC"
-    #subnet_id                     = element(azurerm_subnet.lab01-rsg-subnet.*.id, count.index)
-    subnet_id                     = azurerm_subnet.lab01-rsg-subnet.id
+    name                          = "${var.resource_group_name}-NIC-${count.index}"
+    subnet_id                     = element(azurerm_subnet.lab01-rsg-subnet[*].id, count.index % 4)
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Create NIC Backend Address Pool Association
 resource "azurerm_network_interface_backend_address_pool_association" "lab01-backendaddrpool-association" {
-  # count                   = var.counts
-  # network_interface_id    = element(azurerm_network_interface.lab01-rsg-nic.*.id, count.index)
-  network_interface_id    = azurerm_network_interface.lab01-rsg-nic.id
-  ip_configuration_name   = "${var.resource_group_name}-NIC"
+  count                   = var.counts
+  network_interface_id    = element(azurerm_network_interface.lab01-rsg-nic[*].id, count.index % 4)
+  ip_configuration_name   = "${var.resource_group_name}-NIC-${count.index}"
   backend_address_pool_id = azurerm_lb_backend_address_pool.lab01-lb-addrpool.id
 }
 
@@ -85,7 +83,7 @@ resource "azurerm_network_security_group" "lab01-rsg-nsg" {
 
   security_rule {
     name                       = "DenyDirectAccessFromtheInternet"
-    priority                   = 100
+    priority                   = 1000
     direction                  = "Inbound"
     access                     = "Deny"
     protocol                   = "*"
@@ -95,14 +93,36 @@ resource "azurerm_network_security_group" "lab01-rsg-nsg" {
     destination_address_prefix = "*"
   }
   security_rule {
-    name                       = "AllowInternal"
-    priority                   = 101
+    name                       = "AllowInboundInternal"
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
     destination_port_range     = "*"
     source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+  security_rule {
+    name                       = "AllowOutboundInternal"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+  security_rule {
+    name                       = "AllowHTTPIncome"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
     destination_address_prefix = "10.0.2.0/24"
   }
 }
@@ -115,21 +135,21 @@ data "azurerm_image" "lab01-packer-img" {
 
 # Create Linux VM
 resource "azurerm_virtual_machine" "lab01-rsg-linux-vm" {
-  # count                             = var.counts
-  name                              = "${var.resource_group_name}-vm"
+  count                             = var.counts
+  name                              = "${var.resource_group_name}-vm-${count.index}"
   resource_group_name               = var.resource_group_name
   location                          = var.resource_group_location
   vm_size                           = var.size_vm
 
-  # network_interface_ids = [element(azurerm_network_interface.lab01-rsg-nic.*.id, count.index)]
-  network_interface_ids = [azurerm_network_interface.lab01-rsg-nic.id]
+  # Create NIC per VM
+  network_interface_ids = [element(azurerm_network_interface.lab01-rsg-nic[*].id, count.index)]
 
   storage_image_reference {
     id = "${data.azurerm_image.lab01-packer-img.id}"
   }
 
   storage_os_disk {
-    name              = "linux-vm-osdisk"
+    name              = "linux-vm-osdisk-${count.index}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -150,22 +170,4 @@ resource "azurerm_virtual_machine" "lab01-rsg-linux-vm" {
   }
   availability_set_id = "${data.azurerm_availability_set.lab01-avlset.id}"
 
-}
-
-resource "azurerm_managed_disk" "lab01-managed-disk" {
-  # count                = var.counts
-  name                 = "${var.resource_group_name}-disk"
-  location             = var.resource_group_location
-  resource_group_name  = var.resource_group_name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = 10
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "lab01-vm-data-disk-attach" {
-  # count              = var.counts
-  managed_disk_id    = azurerm_managed_disk.lab01-managed-disk.id
-  virtual_machine_id = azurerm_virtual_machine.lab01-rsg-linux-vm.id
-  lun                = "10"
-  caching            = "ReadWrite"
 }
